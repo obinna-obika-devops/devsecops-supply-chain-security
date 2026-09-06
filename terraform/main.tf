3ce0e93b9badb989c9257f9ecf1cd6c4650c8e79
OBINNA OBIKA <bilolo74@gmail.com>
2026-09-06 09:21:16 -0400

    fix: define least-privilege KMS key policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,10 +13,29 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
 resource "aws_kms_key" "ecr" {
   description             = "KMS key for secure demo ECR repository"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableAccountAdministration"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_ecr_repository" "secure_demo" {
@@ -34,6 +54,7 @@ resource "aws_ecr_repository" "secure_demo" {
 
 resource "aws_ecr_lifecycle_policy" "secure_demo" {
   repository = aws_ecr_repository.secure_demo.name
+
   policy = jsonencode({
     rules = [{
       rulePriority = 1
@@ -43,7 +64,9 @@ resource "aws_ecr_lifecycle_policy" "secure_demo" {
         countType   = "imageCountMoreThan"
         countNumber = 20
       }
-      action = { type = "expire" }
+      action = {
+        type = "expire"
+      }
     }]
   })
 }
